b6472729302b6948c59072e9bec4aeae274bc0be
dvn495 <152308538+dvn495@users.noreply.github.com>
2024-02-07 19:23:00 -0500

    advANCE

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 document.getElementById("btn_open").addEventListener("click", open_close_menu);
 
-var side_menu = document.getElementsById("menu_side");
-var btn_open = document.getElementsById("btn_open");
-var body = document.getElementsById("body");
+var side_menu = document.getElementById("menu_side");
+var btn_open = document.getElementById("btn_open");
+var body = document.getElementById("body");
 
 	function open_close_menu(){
 		body.classList.toggle("body_move");
